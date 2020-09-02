e8ef6f08eab5dd4f39c039ebb88cf1e351049380
adaezeodurukwe <adaeze.od@softcom.ng>
2020-09-02 11:55:07 +0100

    setup database connection with postgres/sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
+  }
+}
